8d94325d552bed0bb6b489f8ef9b9487ef33fc0a
gab <gab@iMac27.local>
2019-10-11 23:47:20 +0200

    initial package

--- x16demo.s
+++ x16demo.s
@@ -1,8 +1,69 @@
+z_Regs = $20
+z_HL = z_Regs
+z_L = z_Regs
+z_H = z_Regs+1
+z_DE = z_Regs+4
+z_E = z_Regs+4
+z_D = z_Regs+5
+z_As = z_Regs+6
+
+V_H=$9F20
+V_M=$9F21
+V_L=$9F22
+V_D1=$9F23
+VERA_CTRL=$9F25
 
 ;line of code 10 sys 2064
 .byte   	$0b,$08,$0a,$00,$9e,$32,$30,$36,$34,$00,$00,$00,$00,$00,$00
-            lda  #$47
-            sta z_regs
+            lda  #%00000000
+            sta VERA_CTRL
+            lda #$14
+            ldy #$00
+            ldx #$40
+            jsr SetVeraHML
+            lda #%0000001
+            sta V_D1
+            lda #64
+            sta V_D1
+            lda #64
+            sta V_D1
+
+            ;palette
+            lda #$14
+            ldy #$02
+            ldx #$00
+            jsr SetVeraHML
+
+            ldx #2
+            ldy #0
+            lda #<palette
+            sta z_L
+            lda #>palette
+            sta z_H
+Paletteloop:
+            lda (z_HL),y
+            sta V_D1
+            iny
+            bne Paletteloop
+            inc z_H
+            dex
+            bne Paletteloop
             rts
-; ZEROPAGE
-z_regs=$20  
+SetVeraHML:
+            sta V_H
+            sty V_M
+            stx V_L
+            rts
+palette:
+    .word  $0008
+    .word  $0FF0
+    .word  $00FF
+    .word  $0F00
+    .word  $0001
+    .word  $0008
+    .word  $0FF0
+    .word  $00FF
+    .word  $0F00
+    .word  $0001
+  
+
