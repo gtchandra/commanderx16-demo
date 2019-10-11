6e2f265827a06ed940bae2398e3628e17f315fad
gab <gab@iMac27.local>
2019-10-11 23:03:15 +0200

    update

--- x16demo.s
+++ x16demo.s
@@ -1,5 +1,8 @@
-; ZEROPAGE
-z_regs=$20  
+
+;line of code 10 sys 2064
+.byte   	$0b,$08,$0a,$00,$9e,$32,$30,$36,$34,$00,$00,$00,$00,$00,$00
             lda  #$47
             sta z_regs
             rts
+; ZEROPAGE
+z_regs=$20  
