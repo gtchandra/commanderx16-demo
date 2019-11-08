e57cea5da7b39dd8e08939009f6479e04a0dbda7
gtchandra <gtchandra@me.me>
2019-11-08 11:59:29 +0100

    cleaner version but still messy

--- x16demo.s
+++ x16demo.s
@@ -42,7 +42,7 @@ imgcounter=$35
             VERA_SET_ADDR $00000, 2
             VERA_LOAD_CHAR #$50, #$60, #$e0
 
-;fill the color ram with image data
+            ;fill the color ram with image data
             VERA_SET_ADDR $00001, 2
             lda #<image_data
             sta data_pointer_l
@@ -55,7 +55,7 @@ imgcounter=$35
 l0:         lda #$50
             sta size_l
             jsr vera_load      
-            ;skipping padding data in VERA 
+            ;skipping data in VERA char lines
             lda VERA_ADDR_LO
             adc #$5f
             sta VERA_ADDR_LO
@@ -69,93 +69,18 @@ xs1:
 theend:     
             VERA_SET_ADDR $00001, 0
             VERA_LOOP_CHAR_COLOR #$50, #$60
-            
-            jmp theend
-
-            jsr $ffe4    ;keyboard control
-xxxx:       cmp #$20
-            bne xxxx 
-            jmp theend
-
-
-            ;palette loop area
-pl00:       lda #$02
-            sta size_l
-            lda #$02
-            sta size_h
-            VERA_SET_ADDR $40200,0 ;no auto increment for palette loop
-            lda VERA_data
-            ;pha
-            jsr vera_inc
-            lda VERA_data
-            ;pha
-            VERA_SET_ADDR $40200,0 
-            ;di sicuro il ciclo deve chiudersi 4 locazioni prima da eliminare a size
-            ;poi sarà necessario anche assicurarsi che non avvenga sfasamento di byte che genera colori sbagliati 
-pl01:       ;jsr print
-            jsr vera_inc
-            jsr vera_inc
-            ldx VERA_data
-            jsr vera_inc
-            ldy VERA_data
-            jsr vera_dec
-            jsr vera_dec
-            jsr vera_dec
-            stx VERA_data
-            jsr vera_inc
-            sty VERA_data
-            jsr vera_inc
-            dec size_l
-            dec size_l
-            bne pl01
-            lda size_h
-            beq px0
-            dec size_h
-            jmp pl01
-px0:        
-         
             jsr $ffe4    ;keyboard control
             cmp #$20
-            bne px0     ;no key pressed go to loop
-            jmp pl00
-            rts
-            pla 
-            sta VERA_data
-            jsr vera_inc
-            pla
-            sta VERA_data
-            ;jsr $ffe4    ;keyboard control
-            ;cmp #$20
-            ;bne pl00     ;no key pressed go to loop
-            rts
+            bne theend 
+
 
 vera_inc:   VERA_INC_POINTER  
 vera_dec:   VERA_DEC_POINTER          
 vera_load:  VERA_LOAD_DATA    
-print: 
-            lda size_h
-            and #%00001111
-            ora #%00110000
-            jsr $ffd2
-            lda size_l
-            lsr
-            lsr
-            lsr
-            lsr
-            ora #%00110000
-            jsr $ffd2
-            lda size_l
-            and #%00001111
-            ora #%00110000
-            jsr $ffd2
-            
-            lda #$20
-            jsr $ffd2
-            rts
 
 palette_data:
 .incbin "palette.dat"    
-.asciiz "end palette"
+.asciiz "endpalette"
 image_data:
 .incbin "image.dat"
-bop:    .asciiz "end image"
+bop:    .asciiz "endimage"
