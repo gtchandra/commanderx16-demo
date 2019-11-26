3ecdb3719d79886468e3ce36209aab7f3e1ffad1
gtchandra <gtchandra@me.me>
2019-11-26 10:59:18 +0000

    fixed vera code emu v34. changed vera_load2 macro

--- x16demo-320x240-7.s
+++ x16demo-320x240-7.s
@@ -23,35 +23,56 @@ imgcounter=$35
             ;setup VERA
             lda  #%00000000
             sta VERA_ctrl
-            VERA_SET_ADDR $40040, 1
+            ;next set vera display composer register (former 40040)
+            VERA_SET_ADDR $F0000, 1
             VERA_WRITE #%0000001
             VERA_WRITE #64 ;set vertical scale
             VERA_WRITE #64 ;set horizontal scale
-            ;configure mode 7 :: 8bpp BITMAP
             
-            VERA_SET_ADDR $40000, 1
+            ;turn off layer 1 register
+            VERA_SET_ADDR $F3000, 1
+            VERA_WRITE #$00
+
+            ;configure mode 7 :: 8bpp BITMAP on layer 0 register
+            VERA_SET_ADDR $F2000, 1
             VERA_WRITE #$E1;mode7 enable
+            VERA_SET_ADDR $F2004, 1
             VERA_WRITE #$00; configure 320x200
-            VERA_WRITE #$00
-            VERA_WRITE #$00
-            VERA_WRITE #$00
-            VERA_WRITE #$00
-            VERA_WRITE #$00
-            VERA_WRITE #$00
-            VERA_WRITE #$00
             VERA_WRITE #$00
 
             ;setup VERA END
-            VERA_SET_ADDR $40200,1  ;palette loading
+            VERA_SET_ADDR $F1000,1  ;palette loading in VERA
+           
             lda #<palette_data
             sta data_pointer_l
             lda #>palette_data
             sta data_pointer_h
-            lda #$02+1
+            lda #$01+1
             sta size_h
-            lda #$02+1
+            lda #$ff
             sta size_l
             jsr vera_load
+;tracker---remove           
+            lda $30
+            sta $2003
+            lda $31
+            sta $2004
+            ldy #$00
+            VERA_SET_ADDR $F1000,1  ;palette loading in VERA
+next:       lda VERA_data 
+            sta $2100,y
+            iny
+            bne next
+next2:      lda VERA_data 
+            sta $2200,y
+            iny
+            bne next2
+            lda VERA_data
+            sta $2300
+        
+;tracker---remove            
+            
+
             ;set the file name index to 0 (image0.dat)
             lda #$30
             sta filname+5
@@ -60,13 +81,13 @@ imgcounter=$35
             sta FILENAME_L
             lda #>filname
             sta FILENAME_H
-            lda #$0a
+            lda #end_filname-filname
             sta FILENAMELENGTH
             lda #<image_data
             sta LOADADDR_L
             lda #>image_data
             sta LOADADDR_H
-            VERA_SET_ADDR $00000, 1
+            VERA_SET_ADDR $00000, 1            
 loadloop: 
             LOAD
             ;fill the color ram with image data
@@ -77,9 +98,10 @@ loadloop:
             sta data_pointer_h
             lda #$1f+1
             sta size_h
-            lda #$20+1
+            lda #$3f
             sta size_l
             jsr vera_load    
+
 
             inc filname+5
             lda filname+5
@@ -88,15 +110,29 @@ loadloop:
 
 wait:      jsr $ffe4    ;keyboard control
             cmp #$20
-            bne wait 
+            bne wait
+            VERA_SET_ADDR $00000, 1
+            ldx #$01+1
+            ldy #$3f+1
+lx:         VERA_WRITE #$FF
+            dey
+            bne lx
+            dex 
+            bne lx
+            rts
 
 vera_inc:   VERA_INC_POINTER  
 vera_dec:   VERA_DEC_POINTER          
 vera_load:  VERA_LOAD_DATA2    
 
 .segment "DATA"
-palette_data:
-.incbin "palette.dat"
+
 filname:    
 .asciiz "image0.dat"
+end_filname:
+.byte "paletteloadshere"
+palette_data:
+.incbin "palette.dat"
+.byte $00
+.byte "imagedata"
 image_data:
